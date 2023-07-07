df9d98719c55a89be158cdc5920eb78693af8296
ahmedkalash <ahmedkalash513@gmail.com>
2023-07-08 00:13:53 +0300

    Add Readme file

---
+++ README.md
@@ -0,0 +1,17 @@
+# Scandiweb-Test-Assignment
+
+## How to use:
+
+- Clone the repo.
+- Run `composer install` to install dependencies.
+- Copy the .env.example file and rename it to .env and add your environment variable in it.
+- Create the database and run the `Database/Schema.sql` file to create the database.
+- Inside the public folder run this command to serve the project  `php -S localhost:8080`  you can change the port as you prefer.
+
+
+## Test result:
+ <img src="TestImages/scandiweb_test_result.png" alt="scandiweb test result">
+
+## Live preview:
+
+[I hope it still up](https://scandiwebtaskassignment.000webhostapp.com/)
--- public/index.php
+++ public/index.php
@@ -1,16 +1,18 @@
 <?php
+
 declare(strict_types=1);
 /*** @var $container Illuminate\Container\Container*/
 /*** @var $app \app\core\Application*/
 //phpinfo();
 
-set_exception_handler(function ($exception){
+// Uncomment this to display errors in development mode
+set_exception_handler(function ($exception) {
     return null;
 });
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-$dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__));
+$dotenv = \Dotenv\Dotenv::createImmutable(dirname(__DIR__));
 $dotenv->safeLoad();
 
 require_once __DIR__ . "/../core/bingings.php";
@@ -20,7 +22,7 @@ $routesFiles=[
     'web',
     'api'
 ];
-foreach ($routesFiles as $routesFile){
+foreach ($routesFiles as $routesFile) {
     $routesClosure = include_once \app\core\Application::ROOT_DIR . "/routes/$routesFile.php";
     $container->call($routesClosure, ['app'=>$app]);
 }
